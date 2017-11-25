3ab503080695c1ae76edf4f38cf85b650768a625
Adam Goodkind <a.goodkind@gmail.com>
2017-11-24 22:56:35 -0600

    added ability to parse debate pages

--- extract_debates.py
+++ extract_debates.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import random
 import requests
+from datetime import datetime
 import urllib.parse as urlparse
 
 debate_page = "http://www.presidency.ucsb.edu/debates.php"
 # Each debate transcript starts http://www.presidency.ucsb.edu/ws/index.php?
 debate_prefix = "http://www.presidency.ucsb.edu/ws/index.php?"
+debate_dct_list = []
 
 class Crawler(object):
 
@@ -33,21 +35,15 @@ class Crawler(object):
                 # print("Found link: '" + link + "'")
                 if link.startswith(debate_prefix):
                     page_links.append(link)
-                    # print("Adding link" + link + "\n")
-                # elif link.startswith('/'):
-                #     parts = urlparse.urlparse(self.current_page)
-                #     page_links.append(parts.scheme + '://' + parts.netloc + link)
-                #     print("Adding link " + parts.scheme + '://' + parts.netloc + link + "\n")
-                # else:
-                #     page_links.append(self.current_page+link)
-                #     print("Adding link " + self.current_page+link + "\n")
 
         except Exception: # Magnificent exception handling
             pass
 
+        ### I copied this originally from a stackoverflow answer, ###
+        ### and when I remove this, it screws it up. I don't know ###
+        ### what it does thought                                  ###
         # Update debate_links
         self.debate_links = self.debate_links.union(set(page_links))
-
         # Choose a random url from non-visited set
         self.current_page = random.sample(self.debate_links.difference(self.visited_links), 1)[0]
         self.counter+=1
@@ -59,8 +55,34 @@ class Crawler(object):
             self.open()
 
         for link in self.debate_links:
-            print(link)
+            # print(link)
+            debate_response = requests.get(link)
+            debate_soup = BeautifulSoup(debate_response.content, 'html.parser')
+            ## this works, but returns a list with other info as well
+            # debate_name = debate_soup.find_all('span', attrs={'class':'ver10'})
+            debate_title = debate_soup.find('meta', attrs={'name':'title'})
+            debate_full_title = debate_title['content']
+            # only split on last '-' that separates name and date
+            debate_name_date = debate_full_title.split(':')[1].rsplit("-", 1)
+            # there are a few outliers. we can handle these later
+            if len(debate_name_date) == 2:
+                debate_name_loc = debate_name_date[0].strip().split(' in ')
+                if len(debate_name_loc) == 2:
+                    debate_name = debate_name_loc[0].strip()
+                    debate_loc = debate_name_loc[1].strip()
+                    debate_date_str = debate_name_date[1].strip()
+                    debate_date = datetime.strptime(debate_date_str, '%B %d, %Y').date()
+                    debate_text = debate_soup.find('span', {'class': 'displaytext'}).get_text()
+                    # print(debate_text)
+
+                    debate_dct_list.append({'name': debate_name,
+                                            'date': debate_date,
+                                            'location': debate_loc,
+                                            'text': debate_text})
+            # break
 
 if __name__ == '__main__':
     C = Crawler()
     C.run()
+    for debate in debate_dct_list:
+        print(debate['name'], debate['date'], debate['location'], len(debate['text']))
